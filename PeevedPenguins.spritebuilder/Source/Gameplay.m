e58f7beb2731866cd5ee66170bc061e2c6dedd0d
Federico Silva <FedericoSilva@federicos-mbp.home>
2014-08-14 12:11:22 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -13,4 +13,42 @@
     CCNode *_catapultArm;
 }
 
+
+// called when the CCB file loads completely
+-(void)didLoadFromCCB {
+    // tells screen to accept touches from Client
+    self.userInteractionEnabled = YES;
+
+}
+
+// called everytime there is a touch
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    [self launchPenguin];
+}
+
+-(void)launchPenguin {
+    
+    //loads penguin
+    CCNode* penguin = [CCBReader load:@"Penguin"];
+    
+    //positions the penguin at catapult bowl
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
+    
+    // add the penguin to the physics node
+    [_physicsNode addChild:penguin];
+    
+    // create & apply force to launch the penguin
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [penguin.physicsBody applyForce:force];
+    
+}
+
+
+
+
+
+
+
+
 @end
